999e7af0023fc18237cbd2d80bc80c5a7792a27b
rrtechmart <reza.cpbd@gmail.com>
2023-07-25 20:37:55 +0600

    added vercel.json file

--- index.js
+++ index.js
@@ -65,12 +65,12 @@ async function run() {
       res.send(result);
     })
 
-    app.get('/users/:id', async(req, res)=>{
-      const id = req.params.id;
-      const query = {_id: new ObjectId(id)};
-      const result = await userCollection.findOne(query);
-      res.send(result);
-    })
+    // app.get('/users/:id', async(req, res)=>{
+    //   const id = req.params.id;
+    //   const query = {_id: new ObjectId(id)};
+    //   const result = await userCollection.findOne(query);
+    //   res.send(result);
+    // })
 
     app.post('/users', async (req, res) => {
       const users = req.body;
